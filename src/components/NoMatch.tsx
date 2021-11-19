071f4f7dec947282dac3766925ec881542485cef
wujinghao <wujinghaowork@foxmail.com>
2021-11-19 13:21:22 +0800

    deploy

--- src/components/NoMatch.tsx
+++ src/components/NoMatch.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import NavigationBar from './NavigationBar';
+
 
 function NoMatch() {
     return (
-        <div>页面不存在，请输入正确的地址
-            <NavigationBar/></div>
+        <div>页面不存在，请输入正确的地址</div>
     );
 }
 
