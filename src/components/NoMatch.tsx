ee4fea4b7a9e69383c6aa62b14bf1df7a638d4dc
wujinghao <wujinghaowork@foxmail.com>
2021-11-19 11:24:18 +0800

    deploy

--- src/components/NoMatch.tsx
+++ src/components/NoMatch.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavigationBar from './NavigationBar';
 
 function NoMatch() {
     return (
-        <div>页面不存在，请输入正确的地址</div>
+        <div>页面不存在，请输入正确的地址
+            <NavigationBar/></div>
     );
 }
 
